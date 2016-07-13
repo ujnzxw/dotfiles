19a911614cd9ef0d06edcf11f9b9a80350610705
ujnzxw <zhaoxiaowei.cn@hotmail.com>
2016-07-13 13:55:01 +0800

    update the LN() function; add note for reading app-install/

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 #
 #-----------------------------------------------------------
-# Author       : ujnzxw
+# Author       : ujnzxw - zhaoxiaowei.cn@hotmail.com
 # Description  : install the dotfiles into OS smoothly
 # Date         : 2016-07-11
 # Note         : don't move this script singlly.
 #              : please run this script in the current path
 #-----------------------------------------------------------
 
+#***********************************************************
+# Before run this script, please read README.md            *
+# in ./app-install first                                   *
+#***********************************************************
+
 # get currect directories
 ROOT=$( cd "$( dirname "$0" )" && pwd )
+
+#----------------Sub-functions------------------------------
 
 # @function: Create a soft link
 # @Param:    src_file - the source file
@@ -22,24 +29,41 @@ function LN()
 		echo "Must input 2 parms for func LN()"
 		exit
 	fi
+
 	src_file=$1
 	tar_file=$2
 
 	# check if the file is already exist
 	if [ -f $tar_file ]; then
-		mv $tar_file ${tar_file}.bk.dotfiles
-		echo "File exists!"
-		echo "Run \"mv $tar_file ${tar_file}.bk.dotfiles\""
+		# chcek if the file is a link file
+		if [ -L $tar_file ]; then
+			echo "Link file exists! Remove the link..."
+			echo "Run \"rm $tar_file\""
+			rm $tar_file
+		else
+			echo "File exists! Make a backup..."
+			echo "Run \"mv $tar_file ${tar_file}.bk.dotfiles\""
+			mv $tar_file ${tar_file}.bk.dotfiles
+		fi
 	fi
 
 	# run link cmd
+	echo "----------------------"
+	echo "Make a link: $tar_file -> $src_file"
+	echo "----------------------"
 	ln $src_file -s $tar_file
 }
 
+#--------------------------Main-----------------------------
+
 # create soft link for the dot files
-LN $ROOT/bash/.bash_profile $HOME/.bash_profile
-LN $ROOT/bash/.bashrc 		$HOME/.bashrc
-LN $ROOT/bash/.bashrc.local $HOME/.bashrc.local
-LN $ROOT/vim/.vimrc 		$HOME/.vimrc
-LN $ROOT/vim/.vimrc.plugin  $HOME/.vimrc.plugin
-LN $ROOT/xmodmap/.Xmodmap   $HOME/.Xmodmap
+#--The source file/dir-------------The target file/dir------
+LN $ROOT/bash/.bash_profile        $HOME/.bash_profile
+LN $ROOT/bash/.bashrc              $HOME/.bashrc
+LN $ROOT/bash/.bashrc.local        $HOME/.bashrc.local
+LN $ROOT/vim/.vimrc                $HOME/.vimrc
+LN $ROOT/vim/.vimrc.plugin         $HOME/.vimrc.plugin
+LN $ROOT/xmodmap/.Xmodmap          $HOME/.Xmodmap
+LN $ROOT/mplayer/.mplayer          $HOME/.mplayer
+LN $ROOT/vimperator/.vimperatorrc  $HOME/.vimperatorrc
+
