ca2110e5f3b797b9620ac98ea69ab39e808e144e
ujnzxw <zhaoxiaowei.cn@hotmail.com>
2016-07-24 11:02:33 +0800

    improve install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #
 #-----------------------------------------------------------
 # Author       : ujnzxw - zhaoxiaowei.cn@hotmail.com
-# Description  : install the dotfiles into OS smoothly
+# Description  : install the dotfiles smoothly
 # Date         : 2016-07-11
 # Note         : don't move this script singlly.
 #              : please run this script in the current path
@@ -22,36 +22,35 @@ ROOT=$( cd "$( dirname "$0" )" && pwd )
 # @Param:    src_file - the source file
 #			 tar_file - the target file
 #
-function LN()
+LN()
 {
 	# check parms
 	if [ $# -ne 2 ]; then
-		echo "Must input 2 parms for func LN()"
-		exit
+		echo "Must input 2 parms for func LN()" && exit 1
 	fi
 
-	src_file=$1
+	src_file=${1}
 	tar_file=$2
 
 	# check if the file is already exist
-	if [ -f $tar_file ]; then
+	if [ -f ${tar_file} ]; then
 		# chcek if the file is a link file
-		if [ -L $tar_file ]; then
+		if [ -L ${tar_file} ]; then
 			echo "Link file exists! Remove the link..."
-			echo "Run \"rm $tar_file\""
-			rm $tar_file
+			echo "Run \"rm ${tar_file}\""
+			rm ${tar_file}
 		else
 			echo "File exists! Make a backup..."
-			echo "Run \"mv $tar_file ${tar_file}.bk.dotfiles\""
-			mv $tar_file ${tar_file}.bk.dotfiles
+			echo "Run \"mv ${tar_file} ${tar_file}.bk.dotfiles\""
+			mv ${tar_file} ${tar_file}.bk.dotfiles
 		fi
 	fi
 
 	# run link cmd
 	echo "----------------------"
-	echo "Make a link: $tar_file -> $src_file"
+	echo "Make a link: ${tar_file} -> ${src_file}"
 	echo "----------------------"
-	ln $src_file -s $tar_file
+	ln ${src_file} -sf ${tar_file}
 }
 
 #--------------------------Main-----------------------------
