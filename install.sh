cf58867f54e9aa959fab8ae8cf801387a75ad278
ujnzxw <zhaoxiaowei.cn@hotmail.com>
2016-07-28 11:23:38 +0800

    enhance install.sh

--- install.sh
+++ install.sh
@@ -20,37 +20,37 @@ ROOT=$( cd "$( dirname "$0" )" && pwd )
 
 # @function: Create a soft link
 # @Param:    src_file - the source file
-#			 tar_file - the target file
+#             tar_file - the target file
 #
 LN()
 {
-	# check parms
-	if [ $# -ne 2 ]; then
-		echo "Must input 2 parms for func LN()" && exit 1
-	fi
+    # check parms
+    if [ $# -ne 2 ]; then
+        echo "Must input 2 parms for func LN()" && exit 1
+    fi
 
-	src_file=${1}
-	tar_file=$2
+    src_file=${1}
+    tar_file=$2
 
-	# check if the file is already exist
-	if [ -f ${tar_file} ]; then
-		# chcek if the file is a link file
-		if [ -L ${tar_file} ]; then
-			echo "Link file exists! Remove the link..."
-			echo "Run \"rm ${tar_file}\""
-			rm ${tar_file}
-		else
-			echo "File exists! Make a backup..."
-			echo "Run \"mv ${tar_file} ${tar_file}.bk.dotfiles\""
-			mv ${tar_file} ${tar_file}.bk.dotfiles
-		fi
-	fi
+    # check if the file is already exist
+    if [ -f ${tar_file} ]; then
+        # chcek if the file is a link file
+        if [ -L ${tar_file} ]; then
+            echo "Link file exists! Remove the link..."
+            echo "Run \"rm ${tar_file}\""
+            rm ${tar_file}
+        else
+            echo "File exists! Make a backup..."
+            echo "Run \"mv ${tar_file} ${tar_file}.bk.dotfiles\""
+            mv ${tar_file} ${tar_file}.bk.dotfiles
+        fi
+    fi
 
-	# run link cmd
-	echo "----------------------"
-	echo "Make a link: ${tar_file} -> ${src_file}"
-	echo "----------------------"
-	ln ${src_file} -sf ${tar_file}
+    # run link cmd
+    echo "----------------------"
+    echo "Make a link: ${tar_file} -> ${src_file}"
+    echo "----------------------"
+    ln ${src_file} -sf ${tar_file}
 }
 
 #--------------------------Main-----------------------------
